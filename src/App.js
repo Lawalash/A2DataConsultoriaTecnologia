51bccee8fd5f3a2fc4e7b8fd03ab56e3f7409df5
Ricardo Alexandre Brasil Júnior <lvtlawalash1@gmail.com>
2025-09-06 18:12:03 -0300

    subir

--- src/App.js
+++ src/App.js
@@ -5,35 +5,25 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom
 import Home from './pages/Home/Home';
 import AgendarDemo from './pages/Home/sections/Demo/AgendarDemo'; // ajuste conforme seu arquivo real
 import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
-import BackToTop from './components/BackToTop/BackToTop';
-import WhatsAppFloat from './components/WhatsAppFloat/WhatsAppFloat';
 
 import './styles/global.css';
 
-const LayoutWithHeader = ({ toggleSidebar, isSidebarOpen, closeSidebar }) => (
+const LayoutWithHeader = () => (
   <>
-    <Header toggleSidebar={toggleSidebar} />
-    <Sidebar isOpen={isSidebarOpen} closeSidebar={closeSidebar} />
+    <Header />
     <main>
       <Outlet />
     </main>
-    {/* opcional: <BackToTop /> <WhatsAppFloat /> */}
   </>
 );
 
 function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = React.useState(false);
-
-  const toggleSidebar = () => setIsSidebarOpen(open => !open);
-  const closeSidebar = () => setIsSidebarOpen(false);
-
   return (
     <Router>
       <div className="app">
         <Routes>
           {/* rotas que usam header/layout */}
-          <Route element={<LayoutWithHeader toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} closeSidebar={closeSidebar} />}>
+          <Route element={<LayoutWithHeader />}>
             <Route path="/" element={<Home />} />
             {/* outras páginas que precisam do header */}
           </Route>
@@ -41,7 +31,7 @@ function App() {
           {/* rota que NÃO deve exibir o Header (AgendarDemo) */}
           <Route path="/demo/agendar-demo" element={<AgendarDemo />} />
 
-          {/* rota catch-all / 404 etc */}
+          {/* rota catch-all / 404 etc (adicione se quiser) */}
         </Routes>
       </div>
     </Router>
